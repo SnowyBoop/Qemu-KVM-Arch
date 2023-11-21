cd0a4f08d9c9a364f3311c4cd8d7dfa89448f223
Fishsticks / PureSesh <150125982+puresesh@users.noreply.github.com>
2023-11-21 04:09:40 -0500

    Update virtmanager-install.sh

--- virtmanager-install.sh
+++ virtmanager-install.sh
@@ -1,3 +1,7 @@
-pacman -Syu libvirt dnsmasq dmidecode cpio
+pacman -Syu libvirt dnsmasq dmidecode cpio 
 systemctl start libvirtd
 pacman -Syu virt-manager
+echo ´uri_default = "qemu:///system"´>> /etc/libvirt/libvirt.conf
+virsh net-start default
+virsh net-autostart default
+systemctl restart libvirtd
