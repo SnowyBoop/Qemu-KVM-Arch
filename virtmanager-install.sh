7e6f7868a7cfc7c9308eb132cf8b0781d2f35386
sovii <150125982+soviimeow@users.noreply.github.com>
2024-04-16 10:36:59 +0200

    Update virtmanager-install.sh

--- virtmanager-install.sh
+++ virtmanager-install.sh
@@ -1,7 +1,15 @@
-pacman -Syu libvirt dnsmasq dmidecode cpio 
+echo "updating system"
+pacman -Syu
+echo "installing basics"
+pacman -Sy libvirt dnsmasq dmidecode cpio
+echo "starting libvirt"
 systemctl start libvirtd
-pacman -Syu virt-manager
+echo "installing noob gui xd"
+pacman -Syu virt-manager 
+echo "setting up qemu and libvirt"
 echo 'uri_default = "qemu:///system"' | sudo tee -a /etc/libvirt/libvirt.conf
+echo "cleaning up"
 virsh net-start default
 virsh net-autostart default
 systemctl restart libvirtd
+echo "done :3"
