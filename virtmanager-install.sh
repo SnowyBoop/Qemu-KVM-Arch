444b530c00e0e39b8693087c0cb4300ed8585789
fishsticks <150125982+gitsticks@users.noreply.github.com>
2024-07-03 17:16:05 +0000

    removed braindamage

--- virtmanager-install.sh
+++ virtmanager-install.sh
@@ -1,12 +1,12 @@
 echo "updating system"
 pacman -Syu
 echo "installing basics"
-pacman -Sy libvirt dnsmasq dmidecode cpio polkit qemu qemu-kvm libvirt-daemon libvirt-clients bridge-utils virt-manager
+pacman -S libvirt dnsmasq dmidecode cpio polkit qemu qemu-kvm libvirt-daemon libvirt-clients bridge-utils virt-manager
 echo "starting libvirt"
 systemctl enable libvirtd
 systemctl start libvirtd
 echo "installing noob gui xd"
-pacman -Syu virt-manager 
+pacman -S virt-manager 
 echo "setting up qemu and libvirt"
 echo 'uri_default = "qemu:///system"' | sudo tee -a /etc/libvirt/libvirt.conf
 echo "cleaning up"
